7e0893927ac8dce1925f629a52b9e2f7796c1936
Stanislav001 <stanislav.stanchev@netea.bg>
2023-11-22 08:59:27 +0200

    Objects and classes

---
+++ 07_ObjectsAndClasses/P01_Students/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace P01_Students;
+
+class Program
+{
+    static void Main()
+    {
+        Student student = new Student()
+        {
+            FirstName = "Pesho",
+            LastName = "Ivanov",
+            City="Sofia",
+            Age = 20
+        };
+        student.FirstName = "Dragan";
+
+        Student ivan = new Student("Ivan", "Petrov", "Ruse", 20);
+
+        Student gosho = new Student("Gosho", "Georgiev", "Ruse", 15);
+
+        gosho.SayHello(ivan.FirstName);
+        gosho.IdCard();
+
+        ivan.IdCard();
+    }
+}
---
+++ 07_ObjectsAndClasses/P01_Students/Student.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace P01_Students
+{
+	public class Student
+	{
+		public string FirstName { get; set; }
+
+		public string LastName { get; set; }
+
+		public string City { get; set; }
+
+		public int Age { get; set; }
+
+		public Student()
+		{
+
+		}
+
+		public Student(string firstName, string lastName)
+		{
+			this.FirstName = firstName;
+			this.LastName = lastName;
+		}
+
+		public Student(string firstName, string lastName, string city, int age)
+		{
+			this.FirstName = firstName;
+			this.LastName = lastName;
+			this.City = city;
+			this.Age = age;
+		}
+
+		public void SayHello(string name)
+		{
+			Console.WriteLine($"{this.FirstName} say hello to {name}");
+		}
+
+		public void IdCard()
+		{
+			Console.WriteLine($"First name -> {this.FirstName}, last name -> {this.LastName}, age -> {this.Age}, city -> {this.City}");
+		}
+	}
+}
